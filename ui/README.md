b62fc51512a33fe1f486a9dbbbcf1c21edb07a9b
Guillermo Fernandez <ollita7@gmail.com>
2020-07-29 17:33:58 -0300

    add angular 10 integration

--- ui/README.md
+++ ui/README.md
@@ -1,6 +1,6 @@
-# VikingsLogApp
+# Ui
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.4.
 
 ## Development server
 
--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'vikings-log-app'`, () => {
+  it(`should have as title 'ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('vikings-log-app');
+    expect(app.title).toEqual('ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('vikings-log-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ui app is running!');
   });
 });
