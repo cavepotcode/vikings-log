da6d1288a7828164ba3a003385928a726643f221
paablo <pabloferrari1789@gmail.com>
2021-01-22 15:12:04 -0300

    add socket in environmens file

--- ui/src/environments/environment.qa.ts
+++ ui/src/environments/environment.qa.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://logqa.cavepotlab.com/api/v1/'
+  apiUrl: 'https://logqa.cavepotlab.com/api/v1/',
+  socket:'https://logqa.cavepotlab.com/api/'
 };
