be02a268598f0c5abce402796c06bd5b1ceb2810
paablo <pabloferrari1789@gmail.com>
2021-01-22 15:12:04 -0300

    add socket in environmens file

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://log.cavepotlab.com/api/v1/'
+  apiUrl: 'https://log.cavepotlab.com/api/v1/',
+  socket:'https://log.cavepotlab.com/api/'
 };
